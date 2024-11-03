957c4338a00e9fa3bac34538542075b7edf79bdf
maxrice <mxrice@loyola.edu>
2024-11-03 16:41:00 -0500

    .

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def lines():
         num_lines = int(input('How many lines do you want?: '))
         if num_lines.isdigit:
 
-        num_chars = int(input('How many characters do you want?: '))
-        characters = input('what characters do you want?: ')
-        while count < num_lines:
-            print(characters * num_chars)
-            count += 1
+            num_chars = int(input('How many characters do you want?: '))
+            characters = input('what characters do you want?: ')
+            while count < num_lines:
+                print(characters * num_chars)
+                count += 1
 
 def circle():
         print('Below is a print of the circle:')
@@ -27,7 +27,27 @@ def circle():
               '    \          / \n'
               '     ---___---\n')
 
-A = ('  0    0\n'
+smile = ('  0    0\n'
      '     -   \n'
      '\________/\n')
-lines()
+
+wink = ('  ^   0\n'
+     '    -   \n'
+     '_________\n'
+     '          ')
+bear = ''
+choice = input('What do you want? wink, smile, or bear: ').lower()
+while choice == smile and choice == wink and choice == bear:
+    if choice == smile:
+        print(smile)
+    elif choice == wink:
+        print(wink)
+    #Art from website https://www.asciiart.eu/animals/bears
+    elif choice == bear:
+        print(' __         __')
+        print('/  \.-"""-./  \ ')
+        print('\    -   -    /')
+        print(' |   o   o   |')
+        print(' \  .-''-.   / ')
+        print('' '   -\__Y__/-' '')
+        print('     `---`')
