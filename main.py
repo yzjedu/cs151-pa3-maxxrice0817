f92c6b6aec1c30952a3b39421f09e74eb667a568
maxrice <mxrice@loyola.edu>
2024-11-03 14:23:59 -0500

    .

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 # Code goes here and DO NOT FORGET INTRO COMMENTS
+from logging import DEBUG
+
 
 #prints however many lines user wants
-count = 0
-num_lines = int(input('How many lines do you want?: '))
-while count < num_lines:
-    print()
-    count += 1
+def lines():
+        blank = ''
+        count = 0
+        num_lines = int(input('How many lines do you want?: '))
+        if num_lines.isdigit:
+
+        num_chars = int(input('How many characters do you want?: '))
+        characters = input('what characters do you want?: ')
+        while count < num_lines:
+            print(characters * num_chars)
+            count += 1
+
+def circle():
+        print('Below is a print of the circle:')
+        print(
+              '     ___----___\n'
+              '   /            \   \n'
+              ' /               \ \n'
+              ' |               |\n'
+              ' |               | \n'
+              '  \             / \n'
+              '    \          / \n'
+              '     ---___---\n')
+
+A = ('  0    0\n'
+     '     -   \n'
+     '\________/\n')
+lines()
