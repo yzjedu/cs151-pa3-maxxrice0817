9c4a005a8520d75543df69bd8c0c875a1429b9c6
maxrice <mxrice@loyola.edu>
2024-11-03 13:12:28 -0500

    .

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Code goes here and DO NOT FORGET INTRO COMMENTS
+
+#prints however many lines user wants
+count = 0
+num_lines = int(input('How many lines do you want?: '))
+while count < num_lines:
+    print()
+    count += 1
